04c0a46ba442055895b2fcf1f549e5104bba5dfc
Ruy-u <119630026+Ruy-u@users.noreply.github.com>
2023-01-29 17:42:07 +0300

    1.0.10
    Fixes description strings using harmony

--- 1.4/Source/CyberFlora/CyberFlora/CyberPlant.cs
+++ 1.4/Source/CyberFlora/CyberFlora/CyberPlant.cs
@@ -1,13 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using HarmonyLib;
 using RimWorld;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
 using UnityEngine;
 using Verse;
 
-
 namespace Ruyu.CyberFlora
 {
+    //Extension for xml
+    public class CyberPlantMod : DefModExtension
+    {
+
+    }
+    //Harmony call
+    [StaticConstructorOnStartup]
+    public static class CyberPlantPatch
+    {
+        static CyberPlantPatch()
+        {
+            Harmony val = new Harmony("Ruyu.CyberPlant");
+            val.PatchAll();
+        }
+    }
+    //Main cyberplant class for fertility
     public class CyberPlant : Plant
     {
         public float fertility = 1f;
@@ -25,7 +41,7 @@ namespace Ruyu.CyberFlora
                     return 0f;
                 }
                 return fertility;
-            }
+            } 
         }
         //Override inspection of plant fertility rate
         public override string GetInspectString()
@@ -36,7 +52,8 @@ namespace Ruyu.CyberFlora
                 if (LifeStage == PlantLifeStage.Growing)
                 {
                     stringBuilder.AppendLine("PercentGrowth".Translate(GrowthPercentString));
-                    stringBuilder.AppendLine("GrowthRate".Translate() + ": " + fertility.ToStringPercent());
+                    stringBuilder.AppendLine("GrowthRate".Translate() + ": "
+                        + fertility.ToStringPercent());
                 }
                 else if (LifeStage == PlantLifeStage.Mature)
                 {
@@ -49,6 +66,47 @@ namespace Ruyu.CyberFlora
                 stringBuilder.Append(text);
             }
             return stringBuilder.ToString().TrimEndNewlines();
+        }
+    }
+    //Patch to change descriptions of cyber plants
+    [HarmonyPatch(typeof(ThingDef), nameof(ThingDef.SpecialDisplayStats))]
+    public static class Patch
+    {
+        public static void Postfix(ref IEnumerable<StatDrawEntry> __result, ThingDef __instance)
+        {
+            if (__instance.HasModExtension<CyberPlantMod>())
+                __result = PassthroughMethod(__result, __instance);
+        }
+        public static IEnumerable<StatDrawEntry> PassthroughMethod(IEnumerable<StatDrawEntry> __result, ThingDef __instance)
+        {
+            float minTemp = -273f;
+            float maxTemp = 1000f;
+            float growMinGlow = 0f;
+                foreach (StatDrawEntry entry in __result)
+                {
+                    if (entry.displayOrderWithinCategory == 4152)
+                    {
+                        yield return new StatDrawEntry(StatCategoryDefOf.Basics,
+                            "MinGrowthTemperature".Translate(), minTemp.ToStringTemperature(),
+                            "Stat_Thing_Plant_MinGrowthTemperature_Desc".Translate(), 4152);
+                    }
+                    else if (entry.displayOrderWithinCategory == 4153)
+                    {
+                        yield return new StatDrawEntry(StatCategoryDefOf.Basics,
+                            "MaxGrowthTemperature".Translate(), maxTemp.ToStringTemperature(),
+                            "Stat_Thing_Plant_MaxGrowthTemperature_Desc".Translate(), 4153);
+                    }
+                    else if(entry.displayOrderWithinCategory == 4154)
+                {
+                    yield return new StatDrawEntry(StatCategoryDefOf.Basics,
+                        "LightRequirement".Translate(), growMinGlow.ToStringPercent(), 
+                        "Stat_Thing_Plant_LightRequirement_Desc".Translate(), 4154);
+                }
+                    else
+                    {
+                        yield return entry;
+                    }
+                }
         }
     }
 }
